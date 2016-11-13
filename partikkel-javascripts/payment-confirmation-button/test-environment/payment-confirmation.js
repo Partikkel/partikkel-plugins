ea238ac00d28606e01272358363312457b6d2b61
Jorgen <jorgenryland@gmail.com>
2016-11-13 17:12:58 +0100

    Added timepass clock icon to partikkel confirmation button

--- partikkel-javascripts/payment-confirmation-button/test-environment/payment-confirmation.js
+++ partikkel-javascripts/payment-confirmation-button/test-environment/payment-confirmation.js
@@ -1,10 +1,21 @@
 (function () {
     function insertHtml() {
+    	var timepass = false;
         var partikkelDiv = document.getElementById('partikkel-paid');
+
+        if(!partikkelDiv) {
+        	timepass = true;
+        	var partikkelDiv = document.getElementById('partikkel-timepass');
+        }
         
         if(partikkelDiv) {
-            var html = '<div class="partikkel-fixed"><a href="https://test.partikkel.io/minside/"><div class="partikkel-floating"><img src="https://test.partikkel.io/images/logo-invert.svg" class="partikkel-icon" /><span>BETALT</span></div></a></div>';
-            partikkelDiv.innerHTML = html;
+            var html = '<div class="partikkel-fixed"><a href="https://test.partikkel.io/minside/"><div class="partikkel-floating"><img src="https://test.partikkel.io/images/logo-invert-transparent.svg" class="partikkel-icon" />';
+            if(timepass) {
+            	html += '<img src="https://test.partikkel.io/images/clock.svg" class="partikkel-clock" />';
+            }
+            html += '<span>BETALT</span></div></a></div>';
+
+            partikkelDiv.innerHTML = html;	            
         }
     }
     setTimeout(insertHtml(), 1);
